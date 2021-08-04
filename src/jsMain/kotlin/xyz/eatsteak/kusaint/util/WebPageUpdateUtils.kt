f69090a73da0b60473a45e5b516c613fbe7d4afa
EATSTEAK <me@eatsteak.xyz>
2021-08-04 16:51:11 +0900

    Remove debug logs and add javascript todo message.

--- src/jsMain/kotlin/xyz/eatsteak/kusaint/util/WebPageUpdateUtils.kt
+++ src/jsMain/kotlin/xyz/eatsteak/kusaint/util/WebPageUpdateUtils.kt
@@ -1,5 +1,5 @@
 package xyz.eatsteak.kusaint.util
 
 actual fun updatePage(original: String, updateResponse: String): String {
-    TODO("Not yet implemented")
+    TODO("JavaScript implementation is not ready yet.")
 }
--- src/jvmMain/kotlin/xyz/eatsteak/kusaint/util/WebPageUpdateUtils.kt
+++ src/jvmMain/kotlin/xyz/eatsteak/kusaint/util/WebPageUpdateUtils.kt
@@ -6,22 +6,22 @@ import org.jsoup.parser.Parser
 actual fun updatePage(original: String, updateResponse: String): String {
     val originalDoc = Jsoup.parse(original)
     val updateDoc = Jsoup.parse(updateResponse, "", Parser.xmlParser())
-    println("[INFO] Getting nodes from Document.")
+    // println("[INFO] Getting nodes from Document.")
     val updateData = updateDoc.getElementsByTag("updates").first()?.children()?.first()
-    println("[INFO] Executing update strategy ${updateData?.tagName()}")
+    // println("[INFO] Executing update strategy ${updateData?.tagName()}")
     when(updateData?.tagName()) {
         "full-update" -> {
             val windowId = updateData.attr("windowId")
             val window = originalDoc.select("[id=\"$windowId\"]").first()
             if(window != null) {
-                println("[INFO] Executing full-update on window $windowId")
+                // println("[INFO] Executing full-update on window $windowId")
                 updateData.children().forEach {
                     when(it.tagName()) {
                         "content-update" -> {
                             val contentId = it.id()
                             val target = originalDoc.select("[id=\"$contentId\"]").first()
                             if(target != null) {
-                                println("[INFO] Executing control-update on content $contentId")
+                                // println("[INFO] Executing control-update on content $contentId")
                                 target.html(it.html().replace("<![CDATA[", "").replace("]]>", ""))
                             }
                         }
@@ -34,7 +34,7 @@ actual fun updatePage(original: String, updateResponse: String): String {
             val window = originalDoc.select("[id=\"${windowId}_root_\"]").first()
             val replaces = mutableMapOf<String, String>()
             if(window != null) {
-                println("[INFO] Executing delta-update on window $windowId")
+                // println("[INFO] Executing delta-update on window $windowId")
                 updateData.children().forEach {
                     when(it.tagName()) {
                         "control-update" -> {
@@ -42,7 +42,7 @@ actual fun updatePage(original: String, updateResponse: String): String {
                             val target = window.select("[id=\"$controlId\"]").first()
                             val content = it.getElementsByTag("content").first()
                             if(target != null && content != null) {
-                                println("[INFO] Executing control-update on control $controlId")
+                                // println("[INFO] Executing control-update on control $controlId")
                                 val replacedWith = content.html().replace("<![CDATA[", "").replace("]]>", "")
                                 replaces[controlId] = replacedWith
                                 target.parent()!!.html("<![CDATA[REPLACE_TARGET_$controlId]]>")
