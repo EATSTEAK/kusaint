ad9b3632e3366732081845fa830aa031adb57c17
EATSTEAK <me@eatsteak.xyz>
2021-08-09 16:00:51 +0900

    Implement form parsers for JS.

--- src/jsMain/kotlin/xyz/eatsteak/kusaint/parser/ClientFormParser.kt
+++ src/jsMain/kotlin/xyz/eatsteak/kusaint/parser/ClientFormParser.kt
@@ -1,34 +1,35 @@
 package xyz.eatsteak.kusaint.parser
 
 import io.ktor.http.*
+import org.w3c.dom.ItemArrayLike
 import org.w3c.dom.asList
-import org.w3c.dom.parsing.DOMParser
 import xyz.eatsteak.kusaint.eventqueue.model.SapClient
 import xyz.eatsteak.kusaint.state.State
+import xyz.eatsteak.kusaint.util.JSDOM
 
 actual object ClientFormParser: Parser<String, SapClient> {
 
     actual override suspend fun parse(state: State<String>): SapClient {
         val keys = mutableMapOf<String, String>()
         state.mutations.forEach { actionRes ->
-            if(actionRes.response.contentType()?.equals(ContentType("text", "html")) == true) {
-                println(actionRes.response.status)
-                val doc = DOMParser().parseFromString(actionRes.result, actionRes.response.contentType()!!.toString())
+            if(actionRes.response.contentType().toString().startsWith("text/html")) {
+                val doc = JSDOM(actionRes.result).window.document
                 val clientElem = doc.querySelector("#sap\\.client\\.SsrClient\\.form")
                 if(clientElem != null) {
-                    keys["action"] = clientElem.getAttribute("action")!!
-                    clientElem.children.asList().forEach {
-                        keys[it.id] = it.getAttribute("value")!!
+                    keys["action"] = clientElem.getAttribute("action")!! as String
+                    (clientElem.children as ItemArrayLike<dynamic>).asList().forEach {
+                        keys[(it.id as String)] = (it.getAttribute("value")!! as String)
                     }
                 }
             }
         }
-        return SapClient(
+        val sapClient = SapClient(
             action = keys["action"]!!,
             charset = keys["sap-charset"]!!,
             wdSecureId = keys["sap-wd-secure-id"]!!,
             appName = keys["fesrAppName"]!!,
             useBeacon = (keys["fesrUseBeacon"] == "true")
         )
+        return sapClient
     }
 }
--- src/jsMain/kotlin/xyz/eatsteak/kusaint/parser/SsoFormParser.kt
+++ src/jsMain/kotlin/xyz/eatsteak/kusaint/parser/SsoFormParser.kt
@@ -1,9 +1,19 @@
 package xyz.eatsteak.kusaint.parser
 
 import xyz.eatsteak.kusaint.state.State
+import xyz.eatsteak.kusaint.util.JSDOM
 
 actual object SsoFormParser : Parser<String, SsoForm> {
     actual override suspend fun parse(state: State<String>): SsoForm {
-        TODO("Not yet implemented")
+        val lastMutation = state.mutations.last()
+        val doc = JSDOM(lastMutation.result)
+        val formElem = doc.window.document.querySelector("form[name=\"LoginInfo\"]") ?: throw IllegalStateException("Cannot find LoginInfo form, Is loaded page is sso page?")
+        val inTpBitElem = formElem.querySelector("input[name=\"in_tp_bit\"]") ?: throw IllegalStateException("Cannot find in_tp_bit input, Is sso site is changed?")
+        val rqstCausCdElem = formElem.querySelector("input[name=\"rqst_caus_cd\"]") ?: throw IllegalStateException("Cannot find rqst_caus_cd input, Is sso site is changed?")
+        val ssoForm = SsoForm(
+            inTpBit = (inTpBitElem.getAttribute("value") ?: throw IllegalStateException("Cannot find value of in_tp_bit.")) as String,
+            rqstCausCd = (rqstCausCdElem.getAttribute("value") ?: throw IllegalStateException("Cannot find value of rqst_caus_cd.")) as String
+        )
+        return ssoForm
     }
 }
