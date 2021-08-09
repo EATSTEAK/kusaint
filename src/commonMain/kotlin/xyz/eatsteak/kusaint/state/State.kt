2778b881f6586f4caa9ef4ee3cd1a94750be7361
EATSTEAK <me@eatsteak.xyz>
2021-08-10 00:18:15 +0900

    Refactor TimeTableApi.

--- src/commonMain/kotlin/xyz/eatsteak/kusaint/state/State.kt
+++ src/commonMain/kotlin/xyz/eatsteak/kusaint/state/State.kt
@@ -2,10 +2,13 @@ package xyz.eatsteak.kusaint.state
 
 import xyz.eatsteak.kusaint.action.Action
 import xyz.eatsteak.kusaint.action.ActionResult
+import xyz.eatsteak.kusaint.parser.Parser
 
 interface State<T> {
 
     suspend fun mutate(action: Action<T>): State<T>
+
+    suspend fun <R> parse(parser: Parser<T, R>): R = parser.parse(this)
 
     val mutations: List<ActionResult<T>>
 }
