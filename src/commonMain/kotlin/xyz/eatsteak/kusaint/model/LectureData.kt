024327c07cd4362f3e0d2f5b692814b8de054751
EATSTEAK <me@eatsteak.dev>
2022-03-24 07:23:24 +0900

    refactor: remove predefined actions and events and replaced with WebDynProApplication

--- src/commonMain/kotlin/xyz/eatsteak/kusaint/model/LectureData.kt
+++ src/commonMain/kotlin/xyz/eatsteak/kusaint/model/LectureData.kt
@@ -19,3 +19,20 @@ data class LectureData(
     val seatLeft: String, // 여석
     val lectureTime: String // 강의시간(강의실)
 )
+
+internal fun Map<String, String>.asLectureData(): LectureData = LectureData(
+    getOrElse("계획") { "" },
+    getOrElse("이수구분(주전공)") { "" },
+    getOrElse("이수구분(다전공)") { "" },
+    getOrElse("공학인증") { "" },
+    getOrElse("교과영역") { "" },
+    getOrElse("과목번호") { "" },
+    getOrElse("과목명") { "" },
+    getOrElse("분반") { "" },
+    getOrElse("교수명") { "" },
+    getOrElse("개설학과") { "" },
+    getOrElse("시간/학점(설계)") { "" },
+    getOrElse("수강인원") { "" },
+    getOrElse("여석") { "" },
+    getOrElse("강의시간(강의실)") { "" }
+)
